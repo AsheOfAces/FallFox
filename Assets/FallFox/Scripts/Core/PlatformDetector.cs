a2f985419c1123669bac6f7bce11e043f8739137
Acey <theaidency@gmail.com>
2021-11-18 07:24:36 +0530

    Detect active platform

--- Assets/FallFox/Scripts/Core/PlatformDetector.cs
+++ Assets/FallFox/Scripts/Core/PlatformDetector.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlatformDetector : MonoBehaviour
 {
+    /*This script actively looks for which input device is being used at any given moment. Also checks for the relevant
+    platform in advance to avoid gamepad axis conflicts.
+    */
+
+    [SerializeField] bool StoredInPP = false;
+    [SerializeField] int LinuxWorkAround = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if(PlayerPrefs.GetInt("storageflag") == 1)
+        {
+            StoredInPP = true;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(!StoredInPP)
+        {
+            Debug.Log(Application.platform);
+            if(Application.platform == RuntimePlatform.LinuxEditor || Application.platform == RuntimePlatform.LinuxPlayer)
+            {
+                LinuxWorkAround = 1;
+            }
+        }
     }
 }
